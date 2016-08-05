7dd5c5c850aa9c8b6708e9b18dbc1b0c0d462594
sgaddy <sgaddy@us.ibm.com>
2016-08-05 16:30:33 -0400

    second commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        alert('Left was pressed');
+    }
+    else if(event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+});
